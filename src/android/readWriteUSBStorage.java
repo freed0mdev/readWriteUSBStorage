c673fab546442ab68f00de7d5e1485755aa44058
Dmytro Ivashchenko <yokodima@gmail.com>
2020-01-16 12:47:28 +0200

    dev main

--- src/android/readWriteUSBStorage.java
+++ src/android/readWriteUSBStorage.java
@@ -1,4 +1,4 @@
-package cordova-plugin-read-write-usb-storage;
+package cordova.plugin.read-write-usb-storage;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
